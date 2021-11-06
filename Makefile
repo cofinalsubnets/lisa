f5e8e234719cb446a18873046a8689ca9008d457
gwen <gwendolyn.whelan@gmail.com>
2021-11-06 18:20:27 -0400

    fws

--- Makefile
+++ Makefile
@@ -3,34 +3,32 @@ include config.mk
 n=lips
 b=$n.bin
 
-bin=bin
-docs=share/man/man1/$n.1
-libs=$(sort $(wildcard lib/$n/*.$n))
+docs=share
+libs=lib/lips/prelude.lips
+files=bin/$n $(libs) $(docs)
 
 # command to run the tests
-t=$(bin)/$b -_ $(libs) $(sort $(wildcard test/*))
+t=bin/$b -_ $(libs) $(sort $(wildcard test/*))
 
-test: $(bin)/$b
+test: bin/$b
 	/usr/bin/env TIMEFORMAT="in %Rs" bash -c "time $t"
-$(bin)/%:
-	make -C src ../$@
 
 # install target
 D=$(DESTDIR)$(PREFIX)
 $D/%: %
 	mkdir -p $(dir $@)
-	cp -r $(dir $^) $D/$(firstword $(subst /, ,$^))
+	cp -r $(firstword $^) $(dir $@)
 # install rules
-install: $D/$(bin)/$n $D/$(libs) $D/$(docs)
+install: $(addprefix $D/,$(files))
 uninstall:
-	rm -f $D/$(bin)/$n $D/$(libs) $D/$(docs)
+	rm -f $(addprefix $D/,$(files))
 
 # vim
-#
 V=$(VIMDIR)
-install-vim: $V/syntax/$n.vim $V/ftdetect/$n.vim
+vimfiles=syntax/$n.vim ftdetect/$n.vim
+install-vim: $(addprefix $V/,$(vimfiles))
 uninstall-vim:
-	rm -f $V/{syntax,ftdetect}/$n.vim
+	rm -f $(addprefix $V/,$(vimfiles))
 $V/%/$n.vim: vim/%/$n.vim $V/%
 	cp $^
 $V/%:
@@ -42,17 +40,40 @@ clean:
 	rm -rf `git check-ignore * */*`
 perf: perf.data
 	perf report
-perf.data: $(bin)/$b $(libs)
+perf.data: bin/$b $(libs)
 	perf record ./$t
-valg: $(bin)/$b
+valg: bin/$b
 	valgrind $t
 sloc:
 	cloc --force-lang=Lisp,$n *
-bits: $(bin)/$n $(bin)/$b
+bits: bin/$n bin/$b
 	stat -c "%n %sB" $^
-repl: $(bin)/$n 
+repl: bin/$n
 	rlwrap $^ -_i $(libs) test/00-helpers.lips test/kanren.lips
+
+
+CC ?= gcc
+CPPFLAGS ?= -DPREFIX=\"$(PREFIX)\"
+# fixnums need sign extended bitshifts.
+# other things tend to break TCO ...
+CFLAGS ?= -std=c99 -g -O2 -flto\
+	-Wall -Wstrict-prototypes\
+	-Wno-shift-negative-value\
+	-fno-stack-protector\
+	-fno-unroll-loops
+
+# build config
+#
+n=lips
+b=$n.bin
+# target directory
+# dist binary
+bin/$n: bin/$n.bin
+	strip -o $@ $^
+# dev binary
+bin/$n.bin: $(patsubst %.c,%.o, $(sort $(wildcard src/*.c)))
+	$(CC) $(CFLAGS) $(CPPFLAGS) $(LDFLAGS) -o $@ $^
 
 .PHONY:\
  	test clean perf valg sloc bits install\
- 	install-vim uninstall uninstall-vim repl
+ 	enstall-vim uninstall uninstall-vim repl
--- src/tbl.c
+++ src/tbl.c
@@ -6,101 +6,110 @@
 #include "mem.h"
 
 static Inline u64 hash_bytes(u64 len, char *us) {
- for (u64 h = mix;; h ^= *us++, h *= mix)
-  if (!len--) return h; }
+  for (u64 h = mix;; h ^= *us++, h *= mix)
+    if (!len--) return h; }
 
 static Inline i64 bucket_index(u64 cap, u64 co) {
- return co % cap; }
+  return co % cap; }
 
 static Inline ent bucket(obj t, u64 code) {
- return gettbl(t)->tab[bucket_index(gettbl(t)->cap, code)]; }
+  return gettbl(t)->tab[bucket_index(gettbl(t)->cap, code)]; }
 
 u64 hash(lips v, obj x) {
- switch (kind(x)) {
-  case Sym: return getsym(x)->code;
-  case Str: return hash_bytes(getstr(x)->len, getstr(x)->text);
-  case Two: return hash(v, X(x)) ^ hash(v, Y(x));
-  case Hom: return hash(v, homnom(v, x)) ^ mix;
-  case Num: return rotr64(mix * x, 16);
-  case Vec: return rotr64(mix * V(x)->len, 32);
-  default:  return rotr64(mix * kind(x), 48); } }
+  switch (kind(x)) {
+    case Sym: return getsym(x)->code;
+    case Str: return hash_bytes(getstr(x)->len, getstr(x)->text);
+    case Two: return hash(v, X(x)) ^ hash(v, Y(x));
+    case Hom: return hash(v, homnom(v, x)) ^ mix;
+    case Num: return rotr64(mix * x, 16);
+    case Vec: return rotr64(mix * V(x)->len, 32);
+    default:  return rotr64(mix * kind(x), 48); } }
 
-// rehash(vm, tbl, new_size): destructively resize a hash table.
+// grow(vm, tbl, new_size): destructively resize a hash table.
 // new_size words of memory are allocated for the new bucket array.
 // the old table entries are reused to populate the modified table.
-static obj rehash(lips v, obj t, i64 ns) {
- ent e, ch, *b, *d;
- with(t, set64((mem) (b = cells(v, ns)), 0, ns));
- tbl o = gettbl(t);
- i64 u, n = o->cap;
- d = o->tab; o->tab = b; o->cap = ns;
- while (n--) for (ch = d[n]; ch;
-  e = ch,
-  ch = ch->next,
-  u = bucket_index(ns, hash(v, e->key)),
-  e->next = b[u],
-  b[u] = e);
- return t; }
+static obj grow(lips v, obj t, i64 ns) {
+  ent e, ch, *b, *d;
+  with(t, set64((mem) (b = cells(v, ns)), 0, ns));
+  tbl o = gettbl(t);
+  i64 u, n = o->cap;
+  d = o->tab; o->tab = b; o->cap = ns;
+  while (n--) for (ch = d[n]; ch;
+    e = ch,
+    ch = ch->next,
+    u = bucket_index(ns, hash(v, e->key)),
+    e->next = b[u],
+    b[u] = e);
+  return t; }
 
 // it's possible to shrink a table without allocating any
 // new memory.
-static u0 tblshrink(lips v, obj t) {
- ent e = NULL, f, g;
- tbl u = gettbl(t);
- for (t = 0; t < u->cap; t++)
-  for (f = u->tab[t], u->tab[t] = NULL; f;
-   g = f->next, f->next = e, e = f, f = g);
- for (u->cap >>= 1; e;
-  t = bucket_index(u->cap, hash(v, e->key)),
-  f = e->next,
-  e->next = u->tab[t],
-  u->tab[t] = e,
-  e = f); }
+static u0 shrink(lips v, obj t) {
+  ent e = NULL, f, g;
+  tbl u = gettbl(t);
+  for (t = 0; t < u->cap; t++)
+    for (f = u->tab[t], u->tab[t] = NULL; f;
+      g = f->next,
+      f->next = e,
+      e = f, f = g);
+  for (u->cap >>= 1; e;
+    t = bucket_index(u->cap, hash(v, e->key)),
+    f = e->next,
+    e->next = u->tab[t],
+    u->tab[t] = e,
+    e = f); }
 
-static obj tblade(lips v, obj t, obj k, obj x, i64 bkt) {
- ent e; tbl y;
- with(t, with(k, with(x, e = cells(v, Size(ent)))));
- y = gettbl(t);
- e->key = k, e->val = x;
- e->next = y->tab[bkt], y->tab[bkt] = e;
- ++y->len;
- return x; }
+obj tblset(lips v, obj t, obj k, obj x) {
+  i64 bucket =
+    bucket_index(gettbl(t)->cap, hash(v, k));
+  ent e = gettbl(t)->tab[bucket];
+  while (e)
+    if (eql(e->key, k))
+      return e->val = x;
+    else e = e->next;
 
-obj tblset_s(lips v, obj t, obj k, obj x) {
- i64 b = bucket_index(gettbl(t)->cap, hash(v, k));
- for (ent e = gettbl(t)->tab[b]; e; e = e->next)
-  if (e->key == k) return e->val = x;
- return tblade(v,t,k,x,b); }
+  // allocate an entry
+  with(t, with(k, with(x,
+    e = cells(v, Size(ent)))));
 
-obj tblset(lips v, obj t, obj k, obj val) {
- with(t, val = tblset_s(v, t, k, val));
- if (gettbl(t)->len > 2 * gettbl(t)->cap)
-  with(val, rehash(v, t, gettbl(t)->cap*2));
- return val; }
+  tbl y = gettbl(t);
+  e->key = k;
+  e->val = x;
+  e->next = y->tab[bucket];
+
+  y->tab[bucket] = e;
+  y->len += 1;
+
+  if (y->len > 2 * y->cap)
+    with(x, grow(v, t, y->cap*2));
+
+  return x; }
+
 
 obj tbldel(lips v, obj t, obj k) {
- tbl y = gettbl(t);
- obj r = nil;
- i64 b = bucket_index(y->cap, hash(v, k));
- ent e = y->tab[b];
- struct ent _v = {0,0,e};
- for (ent l = &_v; l && l->next; l = l->next)
-  if (l->next->key == k) {
-   r = l->next->val;
-   l->next = l->next->next;
-   y->len--;
-   break; }
- y->tab[b] = _v.next;
- if (y->len && y->cap > 2 * y->len) with(r, tblshrink(v, t));
- return r; }
+  tbl y = gettbl(t);
+  obj r = nil;
+  i64 b = bucket_index(y->cap, hash(v, k));
+  ent e = y->tab[b];
+  struct ent _v = {0,0,e};
+  for (ent l = &_v; l && l->next; l = l->next)
+    if (l->next->key == k) {
+      r = l->next->val;
+      l->next = l->next->next;
+      y->len--;
+      break; }
+  y->tab[b] = _v.next;
+  if (y->len && y->cap > 2 * y->len)
+    with(r, shrink(v, t));
+  return r; }
 
 obj tblget(lips v, obj t, obj k) {
- for (ent e = bucket(t, hash(v, k)); e; e = e->next)
-  if (eql(e->key, k)) return e->val;
- return 0; }
+  for (ent e = bucket(t, hash(v, k)); e; e = e->next)
+    if (eql(e->key, k)) return e->val;
+  return 0; }
 
 obj table(lips v) {
- tbl t = cells(v, Size(tbl) + 1);
- ent *b = (ent*)(t+1);
- t->len = 0, t->cap = 1, t->tab = b, *b = NULL;
- return puttbl(t); }
+  tbl t = cells(v, Size(tbl) + 1);
+  ent *b = (ent*)(t+1);
+  t->len = 0, t->cap = 1, t->tab = b, *b = NULL;
+  return puttbl(t); }
