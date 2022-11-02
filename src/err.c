a4a44767cbeda685ceb833ee3d4f39de2f347e6a
gwen <gwendolyn.whelan@gmail.com>
2022-11-01 22:46:35 -0400

    show error frame once in backtrace

--- src/err.c
+++ src/err.c
@@ -6,7 +6,7 @@ Vm(xoom) { return Pack(), nope(v, "oom with %d words", v->len); }
 Vm(xary) { return Pack(), nope(v, "takes %d parameters", getnum(xp)); }
 
 #define aubas (((ob*) fp) == v->pool + v->len)
-static NoInline void show_call(la v, mo ip, fr fp) {
+static NoInline void show_call(la v, mo ip, sf fp) {
   fputc('(', stderr);
   la_tx(v, stderr, (ob) ip);
   for (size_t i = 0, argc = fp->argc; i < argc;
@@ -16,12 +16,16 @@ static NoInline void show_call(la v, mo ip, fr fp) {
 #include <stdarg.h>
 static void verrp(la v, const char *msg, va_list xs) {
   mo ip = v->ip;
-  fr fp = v->fp;
+  sf fp = v->fp;
 
   // print error
   fputs("# ", stderr);
   // show the function if there is one
-  if (!aubas) show_call(v, ip, fp), fputc(' ', stderr);
+  if (!aubas)
+    show_call(v, ip, fp),
+    fputc(' ', stderr),
+    ip = fp->retp,
+    fp = fp->subd;
   // show message
   vfprintf(stderr, msg, xs), fputc('\n', stderr);
 
@@ -41,6 +45,6 @@ NoInline ob nope(la v, const char *msg, ...) {
   if (msg) {
     va_list xs;
     va_start(xs, msg), verrp(v, msg, xs), va_end(xs); }
-  v->fp = (fr) (v->sp = v->pool + v->len);
+  v->fp = (sf) (v->sp = v->pool + v->len);
   v->ip = (mo) (v->xp = nil);
   return 0; }
